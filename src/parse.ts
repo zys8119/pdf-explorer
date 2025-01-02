2965a385384a43a8e72901ca059b7257795e13cf
zys8119 <770959294@qq.com>
2025-01-02 15:45:59 +0800

    [add] 解析objs

--- src/parse.ts
+++ src/parse.ts
@@ -1,8 +1,45 @@
+import { only_obj_reg_split, obj_key_reg } from "./regexpConfig";
 export class PdfExplorerParse {
   fileData: Blob = new Blob([]);
+  private content = "";
   constructor() {}
+  private get version() {
+    return /%PDF-(.*)/.exec(this.content)?.[1] || "";
+  }
   async parse() {
-    console.log("parse", this.fileData);
+    this.content = (await this.fileData.text()) || "";
+    const content = this.content;
+    // todo 需要开启新进程，防止影响主线程
+    // 通过endobj分割对象
+    const chunks = [];
+    let start = 0;
+    let tmp = "";
+    while (start < content.length) {
+      if (only_obj_reg_split.test(tmp)) {
+        chunks.push(tmp);
+        tmp = "";
+      } else {
+        tmp += content[start];
+      }
+      start += 1;
+    }
+    if (tmp) {
+      chunks.push(tmp);
+    }
+    // 提取对象信息
+    const objs: Record<string, any> = {
+      otherObjs: [],
+    };
+    chunks.forEach((chunk) => {
+      const match = obj_key_reg.exec(chunk);
+      if (match) {
+        objs[match[2]] = chunk.replace(match[0], "");
+        console.log(match);
+      } else {
+        objs.otherObjs.push(chunk);
+      }
+    });
+    console.log(objs);
   }
 }
 export default PdfExplorerParse;
---
+++ src/regexpConfig.ts
@@ -0,0 +1,2 @@
+export const only_obj_reg_split = /endobj$/;
+export const obj_key_reg = /([^\d]|\n)*(\d+\s*\d+)\s*obj/; // 1 0 obj
